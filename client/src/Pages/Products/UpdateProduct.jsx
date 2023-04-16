f290c82620a0c3267c12108d45f129fa76b07210
moustafargb <moustafargbdev@gmail.com>
2023-04-16 23:33:42 +0200

    update book completed

--- client/src/Pages/Products/UpdateProduct.jsx
+++ client/src/Pages/Products/UpdateProduct.jsx
@@ -1,63 +1,39 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UpdateProduct = () => {
-    const [images, setImages] = useState([]);
-    const [imagesPrvw, setImagesPrvw] = useState([]);
-
-    const navigate = useNavigate();
-
-    const token = localStorage.getItem('token');
-
-    const uploadPics = async (bookId) => {
-        try {
-            const formData = new FormData();
-
-            for (let i = 0; i < images.length; i++) {
-                formData.append('image', images[i]);
-
-                const a = URL.createObjectURL(images[i]);
-            }
-
-            const { data } = await axios.patch(
-                `http://localhost:3000/api/v1/book/addBookPics/${bookId}`,
-                formData,
-                {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-
-            return data;
-        } catch (error) {}
-    };
-
-    const handleUploadFile = (e) => {
-        setImages(e.target.files);
-
-        let b = [];
-        for (let i = 0; i < e.target.files.length; i++) {
-            const a = URL.createObjectURL(e.target.files[i]);
-
-            b.push(a);
-        }
-
-        setImagesPrvw((prev) => [...b]);
-    };
-
-    // * ************************************
+    const { bookId } = useParams();
     const [book, setBook] = useState({
         name: '',
         author: '',
     });
+    const navigate = useNavigate();
 
-    // Add Book
-    const addBook = async (e) => {
+    const token = localStorage.getItem('token');
+
+    const getBookDetails = () => {
+        axios
+            .get(`http://localhost:3000/api/v1/book/id/${bookId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((res) => {
+                const { name, author } = res.data.Books;
+                console.log(name, author);
+                setBook({ ...book, name, author });
+            });
+    };
+
+    useEffect(() => {
+        getBookDetails();
+    }, []);
+
+    // update Book
+    const updateBook = async (e) => {
         const { data } = await axios.post(
-            'http://localhost:3000/api/v1/book/addbook',
+            `http://localhost:3000/api/v1/book/updatebook/${bookId}`,
             book,
             {
                 headers: {
@@ -70,6 +46,7 @@ const UpdateProduct = () => {
 
     // Handle form
     const handleChange = (event) => {
+        console.log(book);
         setBook({
             ...book,
             [event.target.name]: event.target.value,
@@ -78,11 +55,8 @@ const UpdateProduct = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        const data = await addBook();
-        const { Book } = await data;
-        if (Book) {
-            const data = await uploadPics(Book._id);
-
+        const data = await updateBook();
+        if (data) {
             navigate('/products');
         }
     };
@@ -102,6 +76,7 @@ const UpdateProduct = () => {
                     id="form2Example2"
                     name="name"
                     className="form-control"
+                    value={book.name}
                 />
             </div>
 
@@ -116,38 +91,14 @@ const UpdateProduct = () => {
                     name="author"
                     id="form2Example1"
                     className="form-control"
-                />
-            </div>
-            {/* <!-- Email input --> */}
-            <div className="form-outline mb-3">
-                <label className="form-label" htmlFor="form2Example1">
-                    Add images
-                </label>
-                <input
-                    type="file"
-                    multiple
-                    name="image"
-                    onChange={handleUploadFile}
+                    value={book.author}
                 />
             </div>
 
             {/* <!-- Submit button --> */}
             <button className="btn btn-primary btn-block mb-4">
-                Add Book{' '}
+                Update Book{' '}
             </button>
-
-            {imagesPrvw.length > 0 && (
-                <>
-                    <h5>Selected Images</h5>
-                    <ul className="chooseImage">
-                        {imagesPrvw.map((image, index) => (
-                            <li key={index}>
-                                <img src={image} alt="" className="img" />
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
         </form>
     );
 };
