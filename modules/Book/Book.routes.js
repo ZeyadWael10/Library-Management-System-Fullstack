0fb85caee97a6e08c4b6c09c62cf75bbf445373e
ZeyadWael10 <zeyad.abuarab@outlook.com>
2023-04-30 12:19:28 +0200

    User Borrowed Books

---
+++ modules/Book/Book.routes.js
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+import { auth } from '../../Middlewares/Authentication.js';
+import { myMulter, validObject } from '../../Services/Multer.js';
+import { asyncHandler } from '../../Utils/ErrorHandling.js';
+import * as BookController from './Book.controller.js';
+import * as validators from './Book.validation.js';
+import { ValidationFunction } from '../../Middlewares/Validation.js';
+const router = Router();
+router.get('/', BookController.getAllBooks);
+router.get('/id/:_id', BookController.getBookById);
+router.post(
+    '/addbook',
+    ValidationFunction(validators.addBookValidatinon),
+    asyncHandler(BookController.addBook)
+);
+router.post(
+    '/updatebook/:BookId',
+    ValidationFunction(validators.updateBookValidatinon),
+    asyncHandler(BookController.updateBook)
+);
+router.delete(
+    '/deletebook/:BookId',
+    ValidationFunction(validators.deleteBookValidatinon),
+    asyncHandler(BookController.deleteBook)
+);
+router.put(
+    '/borrowbook/:bookId',
+    auth(),
+    ValidationFunction(validators.borrowBookValidatinon),
+    asyncHandler(BookController.borrowBook)
+);
+router.put(
+    '/returnbook/:bookId',
+    auth(),
+    ValidationFunction(validators.returnBookValidatinon),
+    asyncHandler(BookController.returnBook)
+);
+router.get(
+    '/search/issued/:bookname',
+    ValidationFunction(validators.searchIssuedBooksValidatinon),
+    asyncHandler(BookController.searchIssuedBooks)
+);
+router.get(
+    '/search/borrowed/:bookname/:borrowingDate',
+    ValidationFunction(validators.searchBorrowedBooksValidatinon),
+    asyncHandler(BookController.searchBorrowedBooks)
+);
+router.patch(
+    '/addBookPics/:BookId',
+    myMulter({ validation: validObject }).array('image'),
+    asyncHandler(BookController.uploadBookPicture)
+);
+export default router;
---
+++ modules/User/User.routes.js
@@ -0,0 +1,71 @@
+import { Router } from 'express';
+import { auth } from '../../Middlewares/Authentication.js';
+import { ValidationFunction } from '../../Middlewares/Validation.js';
+import * as validators from './User.validation.js';
+import * as UserContoller from './User.controller.js';
+import { asyncHandler } from '../../Utils/ErrorHandling.js';
+import { myMulter, validObject } from '../../Services/Multer.js';
+const router = Router();
+router.post(
+    '/signup',
+    ValidationFunction(validators.signUpValidatinon),
+    asyncHandler(UserContoller.signUp)
+);
+router.get(
+    '/confirmemail/:token',
+    ValidationFunction(validators.confirmEmailValidation),
+    asyncHandler(UserContoller.confirmEmail)
+);
+router.post(
+    '/login',
+    ValidationFunction(validators.loginValidation),
+    asyncHandler(UserContoller.logIn)
+);
+router.post(
+    '/logout',
+    auth(),
+    ValidationFunction(validators.logoutValidation),
+    asyncHandler(UserContoller.logout)
+);
+router.delete(
+    '/deleteuser',
+    auth(),
+    ValidationFunction(validators.deleteUserValidation),
+    asyncHandler(UserContoller.deleteUser)
+);
+router.patch(
+    '/updatepassword',
+    auth(),
+    ValidationFunction(validators.updatePasswordValidation),
+    asyncHandler(UserContoller.updatePasswordAfterLogin)
+);
+router.post(
+    '/forgetpassword',
+    ValidationFunction(validators.sendForgetPasswordEmailValidation),
+    asyncHandler(UserContoller.sendForgetPasswordEmail)
+);
+router.post(
+    '/resetpassword',
+    // ValidationFunction(validators.resetPasswordValidation),
+    asyncHandler(UserContoller.ResetPassword)
+);
+router.put(
+    '/update',
+    auth(),
+    ValidationFunction(validators.updateDataValidation),
+    asyncHandler(UserContoller.updateData)
+);
+router.patch(
+    '/addProfilePic',
+    myMulter({ validation: validObject.image }).single('image'),
+    auth(),
+    ValidationFunction(validators.uploadUserPicValidation),
+    asyncHandler(UserContoller.uploadUserProfile)
+);
+router.get(
+    '/borrowedbooks',
+    auth(),
+    ValidationFunction(validators.getUserBorrowedBooksValidation),
+    asyncHandler(UserContoller.getUserBorrowedBooks)
+);
+export default router;
